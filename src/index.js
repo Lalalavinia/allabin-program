77f16af1be41670b63c53c7107a13aaab19cef0c
Lalalavinia <63832693+Lalalavinia@users.noreply.github.com>
2021-02-12 17:28:02 +1000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -13,28 +13,6 @@ import './index.css';
   )
 }
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </div>
-  )
-}
-const Part = (props) => {
-  return (
-    <div>
-      Part {props.part.name}, total : {props.part.exercises}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises: {props.number}</p>)
-}
-
 const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({handleClick,text}) => {
@@ -60,24 +38,8 @@ const History = (props) => {
   )
 }*/
 const App = () => {
-  /*const course = {
-    header: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }]
-  }
-  const total = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-
+  /*
+  
   const [counter, setCounter] = useState(0)
   const increaseByOne = () => setCounter(counter + 1)
   const decreaseByOne = () => setCounter(counter - 1)
@@ -97,13 +59,10 @@ const App = () => {
     setRight(right + 1)
   }*/
 
+
   return (
     <div>
-      {/* <div>
-        <Header header={course.header} />
-        <Content parts={course.parts} />
-        <Total number={total} />
-      </div>
+      {/* 
       <div>
         <Display counter={counter} />
         <Button handleClick={increaseByOne} text="Plus" />
